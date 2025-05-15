b7d6f5d6b280c048a4f91449d77d377a535c6015
TharinduShantha98 <tharindushantha4@gmail.com>
2025-05-15 16:20:54 +0530

    fixed issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import Hero from "./components/hero";
-import Experience from "./components/experience";
+
 
 export default function Home() {
   return (
     <div>
-
-      {/* <Experience /> */}
        <Hero />
     </div>
   );
